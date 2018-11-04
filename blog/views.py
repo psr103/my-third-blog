86be7a3c590d99ef346cdd3e720b6d9e0f6ff277
psr103 <sungryool91@gmail.com>
2018-11-05 08:54:26 +0900

    1105

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
-from .forms import PostForm, CommentForm, CreateUserForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView # 오브젝트를 생성하는 뷰 (form 혹은 model과 연결되서 새로운 데이터를 넣을 때 CreateView - generic view를 사용)
-# from django.contrib.auth.forms import UserCreationForm  >>  장고의 기본 회원가입 폼 (ID, PW만 확인한다 - 뒤에서 이메일 추가 커스터미아징 예정)
-from django.core.urlresolvers import reverse_lazy # generic view에서는 reverse_lazy를 사용한다.
+from django.urls import reverse_lazy
+from django.views import generic
+
 
 
 def post_list(request):
@@ -88,13 +91,3 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
-
-# CBV (Class Based View 작성!)
-class CreateUserView(CreateView): # generic view중에 CreateView를 상속받는다.
-    template_name = 'templates/registration/signup.html' # 템플릿은?
-    form_class =  CreateUserForm # 푸슨 폼 사용? >> 내장 회원가입 폼을 커스터마지징 한 것을 사용하는 경우
-    # form_class = UserCreationForm >> 내장 회원가입 폼 사용하는 경우
-    success_url = reverse_lazy('create_user_done') # 성공하면 어디로?
-
-class RegisteredView(TemplateView): # generic view중에 TemplateView를 상속받는다.
-    template_name = 'templates/registration/signup_done.html' # 템플릿은?
